29773b38bed8040ec2361d916f3f7cd203e3bd47
seyha30 <sinseyha1x@gmail.com>
2023-03-13 21:29:55 +0700

    nn

--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+// function component
+// remember props are read only
+function Welcome(props) {
+  return <h1>Hello, {props.name} </h1>;
+}
+
+// class component
+
+class WelcomeMe extends React.Component {
+  render() {
+    return <h1>Hello , {this.props.name}</h1>;
+  }
+}
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <div>
+    <Welcome name={"seyha"} />
+    <Welcome name={"seyha01"} />
+    <WelcomeMe name={"sasa"} />
+    <WelcomeMe name={"sasa01"} />
+  </div>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
